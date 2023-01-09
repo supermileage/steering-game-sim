85ff647b42f33d415646f93f3f2252d2b7626f4c
cosparks <colby.r.sparks@gmail.com>
2023-01-08 22:46:34 -0800

    Workaround for strange gitignore behavior

---
+++ src/Game/GameBase.cpp
@@ -0,0 +1,95 @@
+#include "GameBase.h"
+#include "CollisionHandler.h"
+
+using namespace std::chrono;
+
+GameBase::GameBase(SPI_TFT_ILI9341* tft, InputManager* inputManager) : _tft(tft), _inputManager(inputManager) {
+    _timer = new Timer();
+    _inputManager->setGame(this);
+    _inputManager->setButtonPressedCallback1(&GameBase::handleButtonPressed1);
+    _inputManager->setButtonReleasedCallback1(&GameBase::handleButtonReleased1);
+    _inputManager->setButtonPressedCallback2(&GameBase::handleButtonPressed2);
+    _inputManager->setButtonReleasedCallback2(&GameBase::handleButtonReleased2);
+    _inputManager->setJoystickChangedCallback(&GameBase::handleJoystickChanged);
+    _inputManager->setCloseRequestCallback(&GameBase::_handleCloseRequest);
+
+    _collisionDelegate = new Delegate<GameBase, GameObject::Collision>(this, &GameBase::_handleCollision);
+    CollisionHandler::instance().registerCollisionDelegate(_collisionDelegate);
+}
+
+GameBase::~GameBase() {
+    _run = false;
+    _renderThread.join();
+    delete _timer;
+    delete _collisionDelegate;
+}
+
+void GameBase::init() {
+    _timer->start();
+    _tft->background(BlackTFT);
+    _tft->cls();
+
+    setup();
+
+    _renderThread.start(&GameBase::_runRenderQueue, this);
+}
+
+int GameBase::run() {
+    while (_run) {
+        // handle input
+        _inputManager->run();
+
+        Vec2 curJoystickPos = _inputManager->currentJoystickPos();
+        if (curJoystickPos != _lastJoystickPos) {
+            handleJoystickChanged(curJoystickPos);
+            _lastJoystickPos = curJoystickPos;
+        }
+
+        // run 
+        int64_t currentTime = millis();
+            if (currentTime > _lastFrameMillis + FRAME_RATE_MILLIS) {
+                _deltaT = currentTime - _lastFrameMillis;
+                
+                _tft->clear();  // PORTING: remove this in steering version (render queue will handle all rendering)
+                
+                CollisionHandler::instance().checkCollisions();
+                loop();
+
+                GameObject::drawAll(); // PORTING: remove this in steering version (render queue will handle all rendering)
+                _tft->display(); // PORTING: remove this in steering version (render queue will handle all rendering)
+
+                _lastFrameMillis = currentTime;
+            }
+    }
+    return 1;
+}
+
+int64_t GameBase::millis() {
+    return duration_cast<milliseconds>(_timer->elapsed_time()).count();
+}
+
+int64_t GameBase::micros() {
+    return duration_cast<microseconds>(_timer->elapsed_time()).count();
+}
+
+void GameBase::draw(GameObject* graphic) {
+    _renderQueue.push(graphic);
+}
+
+void GameBase::_handleCloseRequest() {
+    _run = false;
+}
+
+void GameBase::_handleCollision(GameObject::Collision collision) {
+    handleCollision(collision.obj1, collision.obj2);
+}
+
+void GameBase::_runRenderQueue() {
+    while (_run) {
+        while (!_renderQueue.empty()) {
+            _renderQueue.front()->clear();
+            _renderQueue.front()->draw();
+            _renderQueue.pop();
+        }
+    }
+}
---
+++ src/Game/GameBase.h
@@ -0,0 +1,54 @@
+#ifndef _GAME_BASE_H_
+#define _GAME_BASE_H_
+
+#include "mbed.h"
+#include "config.h"
+#include "SPI_TFT_ILI9341.h"
+#include "Timer.h"
+#include "util.h"
+#include "InputManager.h"
+#include "ThreadedQueue.h"
+#include "GameObject.h"
+
+class InputManager;
+
+class GameBase {
+    public:
+        GameBase(SPI_TFT_ILI9341* tft, InputManager* _inputManager);
+        virtual ~GameBase();
+        void init();
+        int run();
+
+    protected:
+        SPI_TFT_ILI9341* _tft;
+        Timer* _timer;
+        Vec2 _lastJoystickPos = { 0, 0 };
+        int64_t _lastFrameMillis = 0;
+        int64_t _deltaT = 0;
+        bool _run = true;
+
+        virtual void setup() = 0;
+        virtual void cleanup() = 0;
+        virtual void loop() = 0;
+        virtual void handleJoystickChanged(Vec2 vec) = 0;
+        virtual void handleCollision(GameObject* obj1, GameObject* obj2) = 0;
+        virtual void handleButtonPressed1() = 0;
+        virtual void handleButtonReleased1() = 0;
+        virtual void handleButtonPressed2() = 0;
+        virtual void handleButtonReleased2() = 0;
+        int64_t millis();
+        int64_t micros();
+        void draw(GameObject* graphic);
+
+    private:
+        InputManager* _inputManager;
+        Command* _collisionDelegate = nullptr;
+        Thread _renderThread;
+        ThreadedQueue<GameObject*> _renderQueue;
+        
+        void _handleCloseRequest();
+        void _handleCollision(GameObject::Collision collision);
+        void _runRenderQueue();
+};
+
+#endif
